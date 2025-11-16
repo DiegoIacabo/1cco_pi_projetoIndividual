bf0dd74447c4a7584061b2a3570b1440b67b6267
DiegoIacabo <diego.iacabo@sptech.school>
2025-11-16 18:01:06 -0300

    Adição do script e modelagem lógica do banco de dados no repositório

---
+++ src/database/script_projetoIndividual.sql
@@ -0,0 +1,71 @@
+create database individual;
+use individual;
+
+create table plataforma (
+	idPlataforma int primary key auto_increment,
+    nome varchar(45)
+);
+
+create table tipoJogo (
+	idTipoJogo int primary key auto_increment,
+    nome varchar(45),
+    descricao varchar(200)
+);
+
+create table usuario (
+	idUsuario int primary key auto_increment,
+    nome varchar(45),
+    genero varchar(10),
+    nascimento date,
+    email varchar(45),
+    senha varchar(45),
+	fkPlataforma int,
+    constraint fkPlataformaPreferida
+		foreign key (fkPlataforma)
+			references plataforma(idPlataforma),
+	fkTipoJogo int,
+    constraint fkTipoJogoFavorito
+		foreign key (fkTipoJogo)
+			references tipoJogo(idTipoJogo)
+);
+
+create table quiz (
+	idQuiz int primary key auto_increment,
+    nome varchar(45)
+);
+
+create table questao (
+	idQuestao int primary key auto_increment,
+    pergunta varchar (200),
+    resposta varchar (45),
+    fkQuiz int,
+    constraint fkQuizQuestao
+		foreign key (fkQuiz)
+			references quiz(idQuiz)
+);
+
+create table tentativa (
+	idTentativa int,
+    fkQuiz int,
+    constraint fkQuizTentativa
+		foreign key (fkQuiz)
+			references quiz(idQuiz),
+    fkUsuario int,
+    constraint fkUsuarioTentativa
+		foreign key (fkUsuario)
+			references usuario(idUsuario),
+	constraint pkComposta
+		primary key (idTentativa, fkQuiz, fkUsuario)
+);
+
+insert into plataforma values
+	(default, 'Celular'),
+    (default, 'Computador'),
+    (default, 'Console');
+    
+insert into tipoJogo values
+	(default, 'Battle Royale', 'Muitos jogadores competem até restar apenas um vencedor, buscando recursos e sobrevivendo em uma área de jogo que se reduz ao longo da partida.'),
+    (default, 'FPS', 'Jogos de tiro em primeira pessoa, focados em precisão, reflexos rápidos e combates intensos com armas variadas.'),
+    (default, 'RPG', 'O jogador assume um personagem, evolui habilidades e toma decisões que influenciam a história e o mundo do jogo.'),
+    (default, 'MOBA', 'Times competem em uma arena, cada jogador controlando um personagem único para destruir a base inimiga.'),
+    (default, 'Survival', 'Jogador precisa sobreviver com poucos recursos, enfrentando ameaças, explorando o ambiente e gerenciando necessidades básicas.');
